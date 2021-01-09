e6a542780a82add1fae44b507384d5972266839e
MeetSoni <mksoni1627@gmail.com>
2021-01-09 19:36:52 +0530

    update call

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -19,7 +19,7 @@ const AddUser = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    await axios.post("http://localhost:3003/users", user);
+    await axios.post("https://my-json-server.typicode.com/meet96/react-crud/users", user);
     history.push("/");
   };
   return (
--- src/components/users/EditUser.js
+++ src/components/users/EditUser.js
@@ -24,12 +24,12 @@ const EditUser = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    await axios.put(`http://localhost:3003/users/${id}`, user);
+    await axios.put(`https://my-json-server.typicode.com/meet96/react-crud/users/${id}`, user);
     history.push("/");
   };
 
   const loadUser = async () => {
-    const result = await axios.get(`http://localhost:3003/users/${id}`);
+    const result = await axios.get(`https://my-json-server.typicode.com/meet96/react-crud/users/${id}`);
     setUser(result.data);
   };
   return (
