6913cc64805823a59b30cbc176a9032b6e8aba18
MeetSoni <mksoni1627@gmail.com>
2021-01-11 11:23:11 +0530

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,20 +6,13 @@ const About = () => {
       <div className="py-4">
         <h1>About Page</h1>
         <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
+         test text
         </p>
 
         <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
+         Hello Meet
         </p>
-        <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi praesentium laborum dolorum libero maiores!</p>
-      </div>
+        <p className="lead">Hello</p>
     </div>
   );
 };
